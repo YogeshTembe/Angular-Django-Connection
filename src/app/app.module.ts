adb7569f9596a75b40442c1e214ec23867cb9af9
Yogesh Tembe <yogeshtembe80@gmail.com>
2021-02-28 22:26:42 +0530

    eighth deploy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import {HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import * as PlotlyJS from 'plotly.js/dist/plotly.js';
+
 import { PlotgraphComponent } from './plotgraph/plotgraph.component';
 import { ParallaxDirective } from './parallax.directive';
 
